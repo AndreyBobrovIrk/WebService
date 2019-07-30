3ef6212c465bd00e76e3e3008075ac761f392ab1
Andrey Bobrov <andrey.bobrov.irk@gmail.com>
2019-07-31 00:34:32 +0800

    no message

---
+++ WebService/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    public class DataController : ApiController
+    {
+        DataItem[] m_items = new DataItem[] {
+            new DataItem { Code = 1, Value = "111" },
+            new DataItem { Code = 2, Value = "222" },
+            new DataItem { Code = 3, Value = "333" }
+        };
+
+        public IEnumerable<DataItem> Get()
+        {
+            return m_items;
+        }
+    }
+}
